af3967cd55acc7791451a95a42a532f81da50e2f
therealpickle-real <dilpicklereal@proton.me>
2026-09-11 14:12:59 -0400

    Made the .bat file move to the build directory
    self explanatory

--- ASH/compile-windows.bat
+++ ASH/compile-windows.bat
@@ -1,4 +1,4 @@
 set bat_path=%~dp0
-start /d "" g++.exe -o %bat_path%\src\main.cpp -o main.exe
+start /d "" g++.exe -o %bat_path%\src\main.cpp -o %bat_path%\build\main.exe
 
 REM therealpickle-real: turns out "./" only works for bash
