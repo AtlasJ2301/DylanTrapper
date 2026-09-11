648316bdde0fac240926a7c1e7a1f307bfdd2462
therealpickle-real <dilpicklereal@proton.me>
2026-09-11 11:46:15 -0400

    Simplify compile-windows.bat by removing duplicate command
    Removed redundant command for compiling main.cpp and added start command.

--- ASH/compile-windows.bat
+++ ASH/compile-windows.bat
@@ -1,5 +1,4 @@
 set bat_path=%~dp0
-
-g++ -o %bat_path%\src\main.cpp -o main.exe
+start /d "" g++.exe -o %bat_path%\src\main.cpp -o main.exe
 
 REM therealpickle-real: turns out "./" only works for bash
